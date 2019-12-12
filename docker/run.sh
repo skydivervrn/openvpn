81b105c7f91cf0fc63c1427e465a5f83f07a2791
Aleksandr Boikov <skydiver.vrn@gmail.com>
2019-12-13 01:18:58 +0300

    added client cer generetion

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 ENTITYNAME=${ENTITYNAME:-server}
+CLIENT_COUNT=${CLIENT_COUNT:-3}
+
 
 display_usage() {
   echo
@@ -23,15 +25,28 @@ client() {
 
 crt() {
   cd /root/openvpn-ca
-  echo "./easyrsa init-pki"
+  echo "Execute command: ./easyrsa init-pki"
   ./easyrsa init-pki
-  echo "./easyrsa build-ca nopass"
+  echo "Execute command: ./easyrsa build-ca nopass"
   ./easyrsa build-ca nopass
-  echo "./easyrsa gen-req ${ENTITYNAME} nopass"
+  echo "Execute command: ./easyrsa gen-req ${ENTITYNAME} nopass"
   ./easyrsa gen-req ${ENTITYNAME} nopass
-  echo "./easyrsa sign-req client ${ENTITYNAME}"
+  echo "Execute command: ./easyrsa sign-req server ${ENTITYNAME}"
   ./easyrsa sign-req server ${ENTITYNAME}
-  cp pki/private/${ENTITYNAME}.key /etc/openvpn/
+  echo "Execute command: ./easyrsa gen-dh"
+  ./easyrsa gen-dh
+  openvpn --genkey --secret ta.key
+  cp pki/private/${ENTITYNAME}.key ca.crt ta.key pki/dh.pem pki/issued/server.crt /etc/openvpn/
+  mkdir -p /etc/openvpn/client-configs/keys
+  chmod -R 700 /etc/openvpn/client-configs
+  for i in {0..$CLIENT_COUNT}
+  do
+    echo "Start creating client${i} cert"
+    ./easyrsa gen-req client${i} nopass
+    ./easyrsa sign-req client client${i}
+    cp pki/private/client${i}.key pki/issued/client${i}.crt ta.key ca.crt /etc/openvpn/client-configs/keys/client${i}/
+    echo "End creating client${i} cert"
+  done
 }
 
 argument="$1"
