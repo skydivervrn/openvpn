d0b621a17a9827b9c6ab524460a1d7e30025eeef
Aleksandr Boikov <skydiver.vrn@gmail.com>
2019-12-15 19:34:21 +0300

    edited run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-ENTITYNAME=${ENTITYNAME:-server}
 CLIENT_COUNT=${CLIENT_COUNT:-3}
 OPENVPN_PORT=${OPENVPN_PORT:-1194}
 
@@ -14,43 +13,73 @@ client() {
   echo "Client"
 }
 
-crt() {
+make_base_conf() {
+  export EXTERNAL_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  dockerize -template /etc/openvpn/client-configs/base.tmpl.conf:/etc/openvpn/client-configs/base.conf
+}
+
+client_crt() {
   cd /root/openvpn-ca
-  if [[ -f pki/private/ca.key ]]; then
-    echo "FILE: pki/private/ca.key - already exist"
-    server_run
-  else
-    echo "Execute command: ./easyrsa init-pki"
-    ./easyrsa init-pki
-    echo "Execute command: ./easyrsa build-ca nopass"
-    ./easyrsa build-ca nopass
-    echo "Execute command: ./easyrsa gen-req ${ENTITYNAME} nopass"
-    ./easyrsa gen-req ${ENTITYNAME} nopass
-    echo "Execute command: ./easyrsa sign-req server ${ENTITYNAME}"
-    ./easyrsa sign-req server ${ENTITYNAME}
-    echo "Execute command: ./easyrsa gen-dh"
-    ./easyrsa gen-dh
-    openvpn --genkey --secret ta.key
-    cp pki/private/${ENTITYNAME}.key pki/ca.crt ta.key pki/dh.pem pki/issued/server.crt /etc/openvpn/
-    mkdir -p /etc/openvpn/client-configs/keys
-    mkdir -p /etc/openvpn/client-configs/files
-    chmod -R 700 /etc/openvpn/client-configs
-    cp ta.key pki/ca.crt /etc/openvpn/client-configs/keys/
-    export EXTERNAL_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-    dockerize -template /etc/openvpn/client-configs/base.tmpl.conf:/etc/openvpn/client-configs/base.conf
-    for i in $(seq 1 $CLIENT_COUNT); do
-      echo "Start creating client${i} cert"
+  make_base_conf
+  for i in $(seq 1 $CLIENT_COUNT); do
+    echo "Start creating client${i} cert"
+    if [[ -f pki/private/client${i}.key ]]; then
+      echo "Client key client${i} exist"
+    else
       ./easyrsa gen-req client${i} nopass
       ./easyrsa sign-req client client${i}
       cp pki/private/client${i}.key pki/issued/client${i}.crt /etc/openvpn/client-configs/keys/
       /etc/openvpn/client-configs/make_config.sh client${i}
       echo "End creating client${i} cert"
-    done
-    echo  "Creation client certificates - complete"
-    server_run
-  fi
-  echo "Something WRONG!!!!!!!"
-  exit 1
+    fi
+  done
+  echo  "Creation client certificates - complete"
 }
 
-crt
+server_crt() {
+  cd /root/openvpn-ca
+  if [[ -d /etc/openvpn/client-configs/keys ]]; then
+    echo "Dir: /etc/openvpn/client-configs/keys exist"
+  else
+    mkdir -p /etc/openvpn/client-configs/keys
+    mkdir -p /etc/openvpn/client-configs/files
+  fi
+  if [[ -f pki/private/ca.key ]]; then
+    echo "File:  pki/private/ca.key - already exist"
+  else
+    echo "Execute command: ./easyrsa init-pki"
+    ./easyrsa init-pki
+    echo "Execute command: ./easyrsa build-ca nopass"
+    ./easyrsa build-ca nopass
+    cp pki/ca.crt /etc/openvpn/
+    cp pki/ca.crt /etc/openvpn/client-configs/keys/
+    chmod -R 700 /etc/openvpn/client-configs
+  fi
+  if [[ -f pki/private/server.key ]]; then
+    echo "File: server.key exist"
+  else
+    echo "Execute command: ./easyrsa gen-req server nopass"
+    ./easyrsa gen-req server nopass
+    echo "Execute command: ./easyrsa sign-req server server"
+    ./easyrsa sign-req server server
+    echo "Execute command: ./easyrsa gen-dh"
+    cp pki/private/server.key pki/issued/server.crt /etc/openvpn/
+  fi
+  if [[ -f pki/dh.pem ]]; then
+    echo "File: pki/dh.pem exist"
+  else
+    ./easyrsa gen-dh
+    cp pki/dh.pem /etc/openvpn/
+  fi
+  if [[ -f ta.key ]]; then
+    echo "File: ta.key exist"
+  else
+    openvpn --genkey --secret ta.key
+    cp ta.key /etc/openvpn/
+    cp ta.key /etc/openvpn/client-configs/keys/
+  fi
+}
+
+server_crt
+client_crt
+server_run
